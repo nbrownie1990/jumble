c38ad1fa81d999bb709f62dfefbeacecdc327b99
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-14 22:50:14 +0200

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,36 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import "./styles/App.scss";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Jumbles from "./components/jumbles";
+import NavBar from "./components/navbar";
 
 function App() {
   useEffect(() => {
     AOS.init();
   });
   return (
-    <main className="px-3">
-      <h1> Erste Schritte </h1>
-      <p className="lead">Make it your own.</p>
-
-      <p className="lead">
-        <a
-          href="http://localhost:3000/"
-          className="btn btn-lg btn-secondary fw-bold border-black bg-black"
+    <React.Fragment>
+      <NavBar />
+      <main className="px-3 m-5">
+        <p className="lead">
+          <a
+            href="http://localhost:3000/"
+            className="btn btn-lg btn-secondary fw-bold border-black bg-black"
+          >
+            Learn more
+          </a>
+        </p>
+        <div
+          className="alert alert-primary shadow-lg"
+          role="alert"
+          data-aos="fade-up"
         >
-          Learn more
-        </a>
-      </p>
-      <div
-        className="alert alert-primary shadow-lg"
-        role="alert"
-        data-aos="fade-up"
-      >
-        <Jumbles />- From bottom!
-      </div>
-    </main>
+          <Jumbles /> - From bottom!
+        </div>
+        <p className="lead ">Make it your own.</p>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav
+        className="navbar navbar-expand-lg navbar-dark fixed-top py-4 d-block m-3"
+        data-navbar-on-scroll="data-navbar-on-scroll"
+      >
+        <div className="container">
+          <a className="navbar-brand" href="index.html">
+            <img src="./logo.svg" height="24" alt="Logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"> </span>
+          </button>
+          <div
+            className="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
+              <li className="nav-item px-2" data-anchor="data-anchor">
+                <a
+                  className="nav-link fw-medium active"
+                  aria-current="page"
+                  href="#home"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item px-2" data-anchor="data-anchor">
+                <a class="nav-link" href="#aboutUs">
+                  About us
+                </a>
+              </li>
+              <li className="nav-item px-2" data-anchor="data-anchor">
+                <a className="nav-link" href="#clients">
+                  Clients
+                </a>
+              </li>
+              <li className="nav-item px-2" data-anchor="data-anchor">
+                <a className="nav-link" href="#faq">
+                  Faq
+                </a>
+              </li>
+            </ul>
+            <form className="ps-lg-5">
+              <a className="btn btn-primary order-1 order-lg-0" href="#!">
+                CONTACT US
+              </a>
+            </form>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
