dc84e3a83f79381af61722408741ab356881d951
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-15 11:27:09 +0200

    create folder-structure for pages & add pagesindex

---
+++ frontend/src/pages/pagesindex.js
@@ -0,0 +1,21 @@
+import { AboutUs } from './AboutUs/about-us'
+import { Categories } from './Categories/categories'
+import { CreateJumble } from './CreateJumble/create-jumble'
+import { Home } from './Home/home'
+import { Jumble } from './Jumble/jumble'
+import { Jumbles } from './Jumbles/jumbles'
+import { Login } from './Login/login'
+import { Profile } from './Profile/profile'
+import { SignUp } from './SignUp/sign-up'
+
+export {
+  AboutUs,
+  Categories,
+  CreateJumble,
+  Home,
+  Jumble,
+  Jumbles,
+  Login,
+  Profile,
+  SignUp,
+}
