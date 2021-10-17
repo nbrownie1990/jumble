0a1ccbb66b4a2563f6aa4fed0bdcdbb99721a62c
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-17 22:19:16 +0200

    change styles

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">Footer</div>
+    <footer className="container">
+      <p className="m-2 text-small pt-1 position-absolute bottom-0 end-0">
+        © 2021 All rights reserved.
+        <a href="#" className="text-small text-white mx-2 footer-link ">
+          Impressum{' '}
+        </a>
+      </p>
     </footer>
   )
 }
