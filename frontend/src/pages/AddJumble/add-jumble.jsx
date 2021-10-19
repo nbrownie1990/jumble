820f514b6ccdb0b876eebc1ac2f779ce6cf37a9b
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-19 22:41:38 +0200

    build some more details on add-jumble

--- frontend/src/pages/AddJumble/add-jumble.jsx
+++ frontend/src/pages/AddJumble/add-jumble.jsx
@@ -1,78 +1,120 @@
 import React from 'react'
 import NavBar from '../../components/navbar'
+import Star from '../../components/common/rating'
 
 const AddJumble = () => {
   return (
     <React.Fragment>
       <NavBar />
 
-      <div className="container">
-        <div class="card bg-light">
-          <div className="card-heading">Add Jumble</div>
-          <div className="card-body">
-            <div class="product-content product-wrap">
-              <div class="row">
-                <div class="col-md-5 col-sm-12 col-xs-12">
-                  <img
-                    src="https://via.placeholder.com/194x228/87CEFA"
-                    class="img-responsive"
+      <div className="container rounded bg-white m-5">
+        <div className="row">
+          <div className="col-md-2 border-right">
+            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+              <img
+                className="rounded-circle mt-5"
+                src="https://www.deutschlandmalanders.com/wp-content/uploads/2020/10/Flohschanze-Flohmarkt-Schanzenviertel-Hamburg-1.jpg"
+                width="110"
+              />
+              <span className="font-weight-bold mt-2 mb-3">
+                Bild des Jumbles
+              </span>
+              <span class="form-text">
+                Jumbles sind Orte für dein Ausrangiertes! Es geht um:
+                Food-Sharing, Flea Markets, Clothing Donations & Second-Hand in
+                deiner Nähe!
+              </span>
+            </div>
+          </div>
+          <div className="col-md-5 border-right">
+            <div className="p-3 py-5">
+              <div className="d-flex justify-content-between align-items-center mb-3">
+                <h2 className="text-right">Erstelle ein Jumble</h2>
+              </div>
+              <div className="row mt-2">
+                <div className="col-md-6">
+                  <label className="labels">Jumble-Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Jumblename"
+                    value="Flohschanze"
                   />
                 </div>
-                <div class="col-md-7 col-sm-12 col-xs-12">
-                  <div class="product-detail">
-                    <form method="POST">
-                      <div className="input-group">
-                        <input
-                          className="input--style-3"
-                          type="text"
-                          placeholder="Name"
-                          name="name"
-                        />
-                      </div>
-                      <div className="input-group">
-                        <input
-                          className="input--style-3"
-                          type="text"
-                          placeholder="Adresse"
-                          name="Adresse"
-                        />
-                      </div>
-                      <div className="input-group">
-                        <div className="rs-select2">
-                          <select name="gender">
-                            <option disabled="disabled" selected="selected">
-                              Category
-                            </option>
-                            <option>Flea-Market</option>
-                            <option>Second-Hand</option>
-                            <option>FoodSharing</option>
-                          </select>
-                          <div className="select-dropdown"></div>
-                        </div>
-                      </div>
-                      <div className="input-group">
-                        <input
-                          className="input--style-3"
-                          type="text"
-                          placeholder="Termine"
-                          name="Termine"
-                        />
-                      </div>
-                      <div className="input-group">
-                        <input
-                          className="input--style-3"
-                          type="text"
-                          placeholder="Öffnungszeiten"
-                          name="oeffnungszeiten"
-                        />
-                      </div>
-                      <div className="p-t-10">
-                        <button className="btn btn-primary" type="submit">
-                          Submit
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+              </div>
+
+              <div className="row mt-3">
+                <div className="col-md-12">
+                  <label className="labels">Adresse</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Adresse"
+                    value=" Neuer Kamp 30, 20357 Hamburg"
+                  />
+                  <span id="placeHelpInline" class="form-text">
+                    Wo befindet sich das Jumble?
+                  </span>
+                </div>
+
+                <div className="col-md-12">
+                  <label className="labels">Termine</label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    placeholder="Termine"
+                    value="17.05.2022"
+                  />
+                  <span id="dateHelpInline" class="form-text">
+                    Wann findet das nächste Jumble statt?
+                  </span>
+                </div>
+                <div className="col-md-12">
+                  <label className="labels">Öffnungszeiten</label>
+                  <input
+                    type="time"
+                    className="form-control"
+                    placeholder="Öffnungszeiten"
+                    value="10-16 Uhr"
+                  />
+                  <span id="timeHelpInline" class="form-text">
+                    Wie sind die Öffnungszeiten des Jumbles?
+                  </span>
+                </div>
+                <div className="col-md-12">
+                  <label className="labels"> Website</label>
+                  <input
+                    type="url"
+                    className="form-control"
+                    placeholder="Website"
+                    value="www.flohschanze.de"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-5 border-right">
+            <div className="p-3 py-5">
+              <div className="d-flex flex-column align-items-center text-center">
+                <div className="form-group col-md-12">
+                  <label className="labels">Rezension schreiben</label>
+                  <textarea
+                    className="form-control rounded-0"
+                    rows="10"
+                    placeholder="Wie ist/war es hier?"
+                    value="Verkaufe hier einmal im Monat meine Klamotten und es lohnt sich!"
+                  ></textarea>
+                </div>
+
+                <div className="mt-5 text-center">
+                  <Star />
+                  <button
+                    className="btn btn-primary profile-button mt-3"
+                    type="button"
+                  >
+                    Save Jumble
+                  </button>
                 </div>
               </div>
             </div>
--- frontend/src/pages/Profile/edit-profile.jsx
+++ frontend/src/pages/Profile/edit-profile.jsx
@@ -6,101 +6,85 @@ const EditProfile = () => {
     <React.Fragment>
       <NavBar />
 
-      <div className="container rounded bg-white mt-5 mb-5">
+      <div className="container rounded bg-white m-5">
         <div className="row">
-          <div className="col-md-3 border-right">
+          <div className="col-md-2 border-right">
             <div className="d-flex flex-column align-items-center text-center p-3 py-5">
               <img
                 className="rounded-circle mt-5"
                 src="https://i.imgur.com/O1RmJXT.jpg"
-                width="90"
+                width="110"
               />
               <span className="font-weight-bold">John Doe</span>
-              <span className="text-black-50">john_doe12@bbb.com</span>
-              <span>United States</span>
             </div>
           </div>
           <div className="col-md-5 border-right">
             <div className="p-3 py-5">
               <div className="d-flex justify-content-between align-items-center mb-3">
-                <h6 className="text-right">Edit your profile</h6>
+                <h2 className="text-right">Edit your profile</h2>
               </div>
               <div className="row mt-2">
                 <div className="col-md-6">
-                  <label className="labels">Name</label>
+                  <label className="labels">Nutzername</label>
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="first name"
-                    value="John"
-                  />
-                </div>
-                <div className="col-md-6">
-                  <label className="labels">Surname</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    value="Doe"
-                    placeholder="Doe"
+                    placeholder="Nutzername"
+                    value="JohnD123"
                   />
                 </div>
               </div>
+
               <div className="row mt-3">
                 <div className="col-md-12">
-                  <label className="labels">Headline</label>
+                  <label className="labels">E-Mail:</label>
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="headline"
-                    value="UI/UX Developer"
+                    placeholder="E-Mail"
+                    value="john.d@gmail.com"
                   />
+                  <span id="mailHelpInline" class="form-text">
+                    Hier eine valide E-Mail-Adresse eintragen.
+                  </span>
                 </div>
+
                 <div className="col-md-12">
-                  <label className="labels">Current position</label>
+                  <label className="labels">Passwort</label>
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="headline"
-                    value="UI/UX Developer at Boston"
+                    placeholder="Passwort"
+                    value="*******"
                   />
-                </div>
-                <div className="col-md-12">
-                  <label className="labels">Education</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="education"
-                    value="Boston University"
-                  />
+                  <span id="passwordHelpInline" class="form-text">
+                    Muss zwischen 8 und 20 Zeichen haben.
+                  </span>
                 </div>
               </div>
-              <div className="row mt-3">
-                <div className="col-md-6">
-                  <label className="labels">Country</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="country"
-                    value="USA"
-                  />
+            </div>
+          </div>
+
+          <div className="col-md-5 border-right">
+            <div className="p-3 py-5">
+              <div className="d-flex flex-column align-items-center text-center">
+                <div className="form-group col-md-12">
+                  <label className="labels">Erzähl uns etwas von dir</label>
+                  <textarea
+                    className="form-control rounded-0"
+                    rows="10"
+                    placeholder="Erzähl etwas von dir!"
+                    value="Hallo Ihr Lieben! Ich mache regelmäßig Flohmärkte mit, also kommt vorbei! Pieces from all over the world - picked and cared with love for you. ♥️"
+                  ></textarea>
                 </div>
-                <div className="col-md-6">
-                  <label className="labels">State/Region</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    value="Boston"
-                    placeholder="state"
-                  />
+                <div className="mt-5 text-center">
+                  <button
+                    className="btn btn-primary profile-button"
+                    type="button"
+                  >
+                    Save Profile
+                  </button>
                 </div>
-              </div>
-              <div className="mt-5 text-center">
-                <button
-                  className="btn btn-primary profile-button"
-                  type="button"
-                >
-                  Save Profile
-                </button>
               </div>
             </div>
           </div>
