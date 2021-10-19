261181640747eea3158b1433f5cb2793a64ca92b
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-20 01:08:08 +0200

    add logout-msg

--- frontend/src/pages/Logout/logout.jsx
+++ frontend/src/pages/Logout/logout.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Logout = () => {
   return (
-    <main>
+    <main className="m-2 text-center text-primary">
       <h1>Logout</h1>
+      <h2>You are successfully logged-out.</h2>
+      <h3>Come back soon! </h3>
     </main>
   )
 }
