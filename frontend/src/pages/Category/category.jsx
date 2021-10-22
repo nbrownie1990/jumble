0f787b3737f29300f0d25a096d2a87009236a03a
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-22 16:23:51 +0200

    add category

--- frontend/src/pages/Category/category.jsx
+++ frontend/src/pages/Category/category.jsx
@@ -1,7 +1,38 @@
 import React, { Component } from 'react'
 import NavBar from '../../components/navbar'
+import JumbleList from '../../components/common/jumbleList'
+import { getJumbles } from '../../services/fakeJumbleService'
+import { getCategories } from '../../services/fakeCategoryService'
 class Category extends React.Component {
+  state = {
+    jumbles: [],
+    categories: [],
+  }
+
+  componentDidMount() {
+    this.setState({ jumbles: getJumbles(), categories: getCategories() })
+  }
+
+  handleCategorySelect = category => {
+    this.setState({ selectedCategory: category })
+  }
+
+  handleJumbleSelect = jumble => {
+    this.setState({ selectedJumble: jumble })
+  }
   render() {
+    const { length: count } = this.state.jumbles
+    const { selectedCategory, jumbles: allJumbles } = this.state
+
+    if (count === 0) return <p>There are no jumbles in the database</p>
+
+    const filtered =
+      selectedCategory && selectedCategory._id
+        ? allJumbles.filter(j => j.category._id === selectedCategory._id)
+        : allJumbles
+
+    const jumbles = filtered
+
     return (
       <React.Fragment>
         <NavBar />
@@ -9,7 +40,12 @@ class Category extends React.Component {
           <div className="heading">
             <h1>Jumbles:</h1>
           </div>
-          <div id="listings" className="listings"></div>
+          <JumbleList
+            jumbles={jumbles}
+            selectedJumble={this.state.selectedJumble}
+            onJumbleSelect={this.handleJumbleSelect}
+            jumblesCount={filtered.length}
+          />
         </div>
       </React.Fragment>
     )
