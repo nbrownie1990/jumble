ef74377a0fb9fe667872a050a10fa947585caa9b
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-23 15:49:35 +0200

    add backend files with start.spring.io

---
+++ backend/src/main/java/de/nbrownie/jumble/JumbleApplication.java
@@ -0,0 +1,13 @@
+package de.nbrownie.jumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumbleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/nbrownie/jumble/JumbleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nbrownie.jumble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
