d86de5b773c5a4179993eabf1e970222f43a9e63
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-25 22:44:39 +0200

    add dockerfile &-image

--- backend/src/main/java/de/nbrownie/jumblesbackend/JumblesbackendApplication.java
+++ backend/src/main/java/de/nbrownie/jumblesbackend/JumblesbackendApplication.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
+
 public class JumblesbackendApplication implements CommandLineRunner {
+
 
 	@Autowired
 	private UserRepository userRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JumblesbackendApplication.class, args);
 	}
 
