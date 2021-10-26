8f0516ce1259fe7048763049484875e530b4d238
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-26 20:57:34 +0200

    change usercontroller

--- backend/src/main/java/de/nbrownie/jumblesbackend/controllers/UserController.java
+++ backend/src/main/java/de/nbrownie/jumblesbackend/controllers/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/api")
 public class UserController {
     @Autowired
