6400186e916a55cfebd8c664883f7e716d90f456
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-26 21:11:18 +0200

    delete redundant code

--- backend/src/main/java/de/nbrownie/jumblesbackend/controllers/UserController.java
+++ backend/src/main/java/de/nbrownie/jumblesbackend/controllers/UserController.java
@@ -7,7 +7,7 @@ import java.math.BigInteger;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
