e7a3d62a0cc35a1d2ba4e0391108075d83db2dd0
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-26 17:50:21 +0200

    delete some redundant code

--- backend/src/main/java/de/nbrownie/jumblesbackend/JumblesbackendApplication.java
+++ backend/src/main/java/de/nbrownie/jumblesbackend/JumblesbackendApplication.java
@@ -1,13 +1,7 @@
 package de.nbrownie.jumblesbackend;
-
-import de.nbrownie.jumblesbackend.models.User;
-import de.nbrownie.jumblesbackend.repo.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 
 @SpringBootApplication
 public class JumblesbackendApplication {
