36d52275d5d440b245756345622aa1083114d3fc
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-26 17:50:02 +0200

    change sign-up to run localhost again

--- frontend/src/pages/SignUp/sign-up.jsx
+++ frontend/src/pages/SignUp/sign-up.jsx
@@ -1,47 +1,45 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { useAuth } from '../../auth/AuthProvider'
-import {postNewUser} from "../../services/authService";
+// import { useAuth } from '../../auth/AuthProvider'
+// import {postNewUser} from "../../services/authService";
 
-const initialState = {
-  username: '',
-  email: '',
-  password: '',
-}
+// const initialState = {
+//   username: '',
+//   email: '',
+//   password: '',
+// }
 
 export default function SignUp() {
-  const { login, user } = useAuth()
-  const [credentials, setCredentials] = useState(initialState)
-  const [passwordCheck, setPasswordCheck] = useState('')
-  const [error, setError] = useState()
-  const [loading, setLoading] = useState(false)
-  const history = useHistory()
+  // const { login, user } = useAuth()
+  // const [credentials, setCredentials] = useState(initialState)
+  // const [passwordCheck, setPasswordCheck] = useState('')
+  // const [error, setError] = useState()
+  // const [loading, setLoading] = useState(false)
+  // const history = useHistory()
 
-  const handleCredentials = event => {
-      setCredentials({
-          ...credentials,
-          [event.target.name]: event.target.value,
-      })
-  }
+  // const handleCredentials = event => {
+  //     setCredentials({
+  //         ...credentials,
+  //         [event.target.name]: event.target.value,
+  //     })
+  // }
 
-  const handlePasswordCheck = event => {
-      setPasswordCheck(event.target.value)
-  }
+  // const handlePasswordCheck = event => {
+  //     setPasswordCheck(event.target.value)
+  // }
 
-  const handleSubmit = event => {
-      event.preventDefault()
-      setLoading(true)
-      setError()
-      postNewUser(credentials)
-          .then(() => login(credentials))
-          .then(() => history.push('/profile/view'))
-          .catch(error => {
-              setError(error)
-              setLoading(false)
-          })
-  }
-
-
+  // const handleSubmit = event => {
+  //     event.preventDefault()
+  //     setLoading(true)
+  //     setError()
+  //     postNewUser(credentials)
+  //         .then(() => login(credentials))
+  //         .then(() => history.push('/profile/view'))
+  //         .catch(error => {
+  //             setError(error)
+  //             setLoading(false)
+  //         })
+  // }
 
   return (
     <React.Fragment>
@@ -55,16 +53,16 @@ export default function SignUp() {
                 <div className="card-body">
                   <h4 className="card-title">Sign Up</h4>
                   <form
-                    as="form" onSubmit={handleSubmit}
+                    // as="form" onSubmit={handleSubmit}
                     className="my-login-validation"
                   >
                     <div className="form-group">
                       <label for="name">Name</label>
                       <input
                         name="username"
-                        value={credentials.username}
+                        // value={credentials.username}
                         className="form-control"
-                        onChange={handleCredentials}
+                        // onChange={handleCredentials}
                         title="Username"
                         required
                         autofocus
@@ -75,12 +73,11 @@ export default function SignUp() {
                     <div className="form-group">
                       <label for="email">E-Mail Adresse</label>
                       <input
-                         name="email"
-                         value={credentials.email}
-                         className="form-control"
-                         onChange={handleCredentials}
-                         title="Email"
-                         required
+                        name="email"
+                        //  value={credentials.email}
+                        className="form-control"
+                        //  onChange={handleCredentials}
+                        title="Email"
                         required
                       />
                       <div className="invalid-feedback">
@@ -93,20 +90,20 @@ export default function SignUp() {
                       <input
                         name="password"
                         type="password"
-                        value={credentials.password}
+                        // value={credentials.password}
                         className="form-control"
-                        onChange={handleCredentials}
+                        // onChange={handleCredentials}
                         title="Password"
                         required
                         data-eye
                       />
-                        <TextField
-                            name="passwordCheck"
-                            value={passwordCheck}
-                            onChange={handlePasswordCheck}
-                            title="Retype Password"
-                            type="password"
-                        />
+                      {/* <TextField
+                        name="passwordCheck"
+                        value={passwordCheck}
+                        onChange={handlePasswordCheck}
+                        title="Retype Password"
+                        type="password"
+                      /> */}
                       <div className="invalid-feedback">
                         Passwort ist erforderlich
                       </div>
@@ -134,15 +131,15 @@ export default function SignUp() {
 
                     <div className="form-group m-0">
                       <br />
-                      {credentials.username && credentials.password
-                        && passwordCheck === credentials.password && (
-                        <button
+                      {/* {credentials.username && credentials.password
+                        && passwordCheck === credentials.password && ( */}
+                      <button
                         type="submit"
                         className="btn btn-primary btn-block"
                       >
                         Sign up
                       </button>
-                      )}
+                      {/* )} */}
                     </div>
                     <div className="mt-4 text-center">
                       Hast du bereits einen Account?
@@ -160,5 +157,3 @@ export default function SignUp() {
     </React.Fragment>
   )
 }
-
-export default SignUp
