5ff01bb234dbb3920a8c6e1bbcedeca07270ad21
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-27 10:23:52 +0200

    a new try

--- backend/src/main/java/de/nbrownie/jumblesbackend/models/User.java
+++ backend/src/main/java/de/nbrownie/jumblesbackend/models/User.java
@@ -2,20 +2,11 @@ package de.nbrownie.jumblesbackend.models;
 
 import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigInteger;
 
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+public class User { 
     private BigInteger user_id;
     private String user_name;
     private String user_email;
--- backend/src/main/java/de/nbrownie/jumblesbackend/repo/UserRepository.java
+++ backend/src/main/java/de/nbrownie/jumblesbackend/repo/UserRepository.java
@@ -1,12 +1,12 @@
-package de.nbrownie.jumblesbackend.repo;
+// package de.nbrownie.jumblesbackend.repo;
 
-import de.nbrownie.jumblesbackend.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import de.nbrownie.jumblesbackend.models.User;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
+// import java.math.BigInteger;
 
 
-@Repository
-public interface UserRepository extends JpaRepository<User, BigInteger> {
-}
+// @Repository
+// public interface UserRepository extends JpaRepository<User, BigInteger> {
+// Optional<UserEntity> findByUsername(String username); }
