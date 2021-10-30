294810980a5c035ffd879d3e1b8e83ae3068bad0
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-30 17:37:40 +0200

    add header to navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,26 +4,28 @@ import { NavLink } from 'react-router-dom'
 class NavBar extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
-        <Container className="container-fluid mt-3">
-          <Navbar.Brand href="/home">Jumble</Navbar.Brand>
+      <header className="mb-3">
+        <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
+          <Container className="container-fluid mt-3">
+            <Navbar.Brand href="/home">Jumble</Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto justify-content-end w-100 text-end mr-2">
-              <Nav.Link href="/home">Home</Nav.Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto justify-content-end w-100 text-end mr-2">
+                <Nav.Link href="/home">Home</Nav.Link>
 
-              <Nav.Link href="/categories">Categories</Nav.Link>
+                <Nav.Link href="/categories">Categories</Nav.Link>
 
-              <Nav.Link href="/jumbleadd">Add Jumble</Nav.Link>
+                <Nav.Link href="/jumbleadd">Add Jumble</Nav.Link>
 
-              <Nav.Link href="/profile">Profile</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
 
-              <Nav.Link href="/logout">Logout</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+                <Nav.Link href="/logout">Logout</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     )
   }
 }
