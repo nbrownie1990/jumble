3c9f54db928b2d5fbbc2cc0b6ae843fcaecdc137
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-30 12:46:07 +0200

    change navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,60 +1,41 @@
 import React, { Component, useState, useEffect } from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 class NavBar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark  py-4 d-block m-3">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand " to="/home">
-            Jumble
-          </NavLink>
+      <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
+        <Container className="container-fluid">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link className="navbar-brand " to="/home">
+                Jumble
+              </Nav.Link>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"> </span>
-          </button>
+              <Nav.Link className="nav-link" to="/home">
+                Home
+              </Nav.Link>
 
-          <div
-            className="collapse navbar-collapse mt-4 mt-lg-0"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
-              <li className="nav-item px-2" data-anchor="data-anchor">
-                <NavLink className="nav-link" to="/home">
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item px-2" data-anchor="data-anchor">
-                <NavLink className="nav-link" to="/categories">
-                  Categories
-                </NavLink>
-              </li>
-              <li className="nav-item px-2" data-anchor="data-anchor">
-                <NavLink className="nav-link" to="/jumbleadd">
-                  Add Jumble
-                </NavLink>
-              </li>
-              <li className="nav-item px-2" data-anchor="data-anchor">
-                <NavLink className="nav-link" to="/profile">
-                  Profile
-                </NavLink>
-              </li>
-            </ul>
-            <form className="ps-lg-5">
-              <NavLink className="nav-link" to="/logout">
+              <Nav.Link className="nav-link" to="/categories">
+                Categories
+              </Nav.Link>
+
+              <Nav.Link className="nav-link" to="/jumbleadd">
+                Add Jumble
+              </Nav.Link>
+
+              <Nav.Link className="nav-link" to="/profile">
+                Profile
+              </Nav.Link>
+
+              <Nav.Link className="nav-link" to="/logout">
                 Logout
-              </NavLink>
-            </form>
-          </div>
-        </div>
-      </nav>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     )
   }
 }
