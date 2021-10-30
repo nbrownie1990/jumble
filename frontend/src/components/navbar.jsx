d2026b769a716c74ef3f635c7768a7e039a3abcb
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-10-30 17:20:51 +0200

    change navbar and toggler

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,33 +5,21 @@ class NavBar extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
-        <Container className="container-fluid">
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav>
-              <Nav.Link className="navbar-brand " to="/home">
-                Jumble
-              </Nav.Link>
+        <Container className="container-fluid mt-3">
+          <Navbar.Brand href="/home">Jumble</Navbar.Brand>
 
-              <Nav.Link className="nav-link" to="/home">
-                Home
-              </Nav.Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto justify-content-end w-100 text-end mr-2">
+              <Nav.Link href="/home">Home</Nav.Link>
 
-              <Nav.Link className="nav-link" to="/categories">
-                Categories
-              </Nav.Link>
+              <Nav.Link href="/categories">Categories</Nav.Link>
 
-              <Nav.Link className="nav-link" to="/jumbleadd">
-                Add Jumble
-              </Nav.Link>
+              <Nav.Link href="/jumbleadd">Add Jumble</Nav.Link>
 
-              <Nav.Link className="nav-link" to="/profile">
-                Profile
-              </Nav.Link>
+              <Nav.Link href="/profile">Profile</Nav.Link>
 
-              <Nav.Link className="nav-link" to="/logout">
-                Logout
-              </Nav.Link>
+              <Nav.Link href="/logout">Logout</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
