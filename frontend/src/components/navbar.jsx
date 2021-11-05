af7c18f46173c82be17c62f1772155e145c98d75
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-05 16:28:27 +0100

    try some navbar-configs

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,33 +1,40 @@
 import React, { Component, useState, useEffect } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-class NavBar extends React.Component {
-  render() {
-    return (
-      <header className="mb-3">
-        <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
-          <Container className="container-fluid mt-3">
-            <Navbar.Brand href="/home">Jumble</Navbar.Brand>
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto justify-content-end w-100 text-end mr-2">
-                <Nav.Link href="/home">Home</Nav.Link>
+const NavBar = () => {
+  const [offset, setOffset] = useState(0)
 
-                <Nav.Link href="/categories">Categories</Nav.Link>
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset)
+      console.log('Testtest123')
+    }
+  }, [])
 
-                <Nav.Link href="/jumbleadd">Add Jumble</Nav.Link>
+  console.log(offset)
 
-                <Nav.Link href="/profile">Profile</Nav.Link>
+  return (
+    <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
+      <Container className="container-fluid mt-3">
+        <Navbar.Brand href="/home">Jumble</Navbar.Brand>
 
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    )
-  }
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto justify-content-end w-100 text-end mr-2">
+            <Nav.Link href="/home">Home</Nav.Link>
+
+            <Nav.Link href="/categories">Categories</Nav.Link>
+
+            <Nav.Link href="/jumbleadd">Add Jumble</Nav.Link>
+
+            <Nav.Link href="/profile">Profile</Nav.Link>
+
+            <Nav.Link href="/logout">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default NavBar
