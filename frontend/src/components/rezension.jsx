0e6ece69b5e2324d64bc7c47774b37577c0e12df
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-07 17:33:20 +0100

    change chat

--- frontend/src/components/rezension.jsx
+++ frontend/src/components/rezension.jsx
@@ -11,11 +11,11 @@ class Rezension extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="rez container scroll-list m-2">
-          <div className="chat">
+        <div className="rez container scroll-list">
+          <div className="chat w-100">
             <div className="mine messages">
               <div className=" message last row">
-                <div className="col-6">
+                <div className="col-xs-6 col-12">
                   <img
                     className="rounded-circle mt-5"
                     src="https://i.imgur.com/O1RmJXT.jpg"
@@ -32,21 +32,24 @@ class Rezension extends React.Component {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="yours messages">
-            <div className="message last row">
-              <div className="col-6">
-                <img
-                  className="rounded-circle mt-5"
-                  src="https://images.unsplash.com/photo-1542596594-649edbc13630?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80"
-                  width="50"
-                  alt="This is a jumble"
-                />
+
+            <div className="yours messages">
+              <div className="message last row">
+                <div className="col-xs-6 col-12">
+                  <img
+                    className="rounded-circle mt-5"
+                    src="https://images.unsplash.com/photo-1542596594-649edbc13630?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80"
+                    width="50"
+                    alt="This is a jumble"
+                  />
+                </div>
+                <div className="col-12">
+                  Macht Spaß! <Star />
+                </div>
+                <div className="col-12">
+                  Sehr gute Auswahl an tollen Sachen.
+                </div>
               </div>
-              <div className="col-12">
-                Macht Spaß! <Star />
-              </div>
-              <div className="col-12">Sehr gute Auswahl an tollen Sachen.</div>
             </div>
           </div>
         </div>
