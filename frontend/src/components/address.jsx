3f4dd3ce4c289bc14b2ef1bc62ebb9aca72e228a
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-09 10:07:38 +0100

    import address

---
+++ frontend/src/components/address.jsx
@@ -0,0 +1,66 @@
+import TextField from './textField'
+import React from 'react'
+
+export default function Address({
+  mode,
+  address,
+  handleEditAddress,
+  handleSaveNewAddress,
+  handleSaveAddressChanges,
+  handleAddressInputChange,
+  handleCancel,
+  handleDeleteAddress,
+  readOnly,
+}) {
+  return (
+    <React.Fragment>
+      <TextField
+        name="street"
+        // value={address.street}
+        // onChange={handleAddressInputChange}
+        title="Straße"
+        placeholder="Straße"
+        type="text"
+        // disabled={readOnly}
+      />
+      <TextField
+        name="number"
+        // value={address.number}
+        // onChange={handleAddressInputChange}
+        title="Hausnummer"
+        placeholder="Hausnr."
+        type="text"
+        // disabled={readOnly}
+      />
+
+      <TextField
+        name="zip"
+        // value={address.zip}
+        // onChange={handleAddressInputChange}
+        title="Postleitzahl"
+        placeholder="Postleitzahl"
+        type="text"
+        // disabled={readOnly}
+      />
+      <TextField
+        name="city"
+        // value={address.city}
+        // onChange={handleAddressInputChange}
+        title="Stadt"
+        placeholder="Stadt"
+        type="text"
+        // disabled={readOnly}
+      />
+
+      <TextField
+        name="country"
+        // value={address.country}
+        // onChange={handleAddressInputChange}
+        title="Land"
+        placeholder="Land"
+        type="text"
+        // disabled={readOnly}
+      />
+    </React.Fragment>
+  )
+}
--- frontend/src/components/jumbleForm.jsx
+++ frontend/src/components/jumbleForm.jsx
@@ -2,6 +2,7 @@ import DateField from './dateField'
 import TextField from './textField'
 import TextArea from './textArea'
 import React from 'react'
+import Address from './address'
 
 export default function JumbleForm({}) {
   return (
@@ -9,37 +10,27 @@ export default function JumbleForm({}) {
       <div className="row mt-2">
         <div className="col-md-12">
           <label className="labels">Jumble-Name</label>
-          <TextField type="text" placeholder="Jumblename" value="Flohschanze" />
+          <TextField type="text" placeholder="z.B. Flohschanze" value="" />
         </div>
       </div>
 
       <div className="row mt-3">
-        <div className="col-md-12">
-          <label className="labels">Adresse</label>
-          <TextField
-            type="text"
-            placeholder="Adresse"
-            value=" Neuer Kamp 30, 20357 Hamburg"
-          />
-          <span id="placeHelpInline" class="form-text">
-            Wo befindet sich das Jumble?
-          </span>
+        <div className="col-md-10 col-8 m-2">
+          <label className="labels">Jumble-Adresse</label>
+          <Address />
         </div>
 
         <div className="col-md-12">
-          <label className="labels">Termine</label>
-          <DateField placeholder="Termine" />
+          <label className="labels">Termin</label>
+          <DateField placeholder="Termin" />
           <span id="dateHelpInline" class="form-text">
             Wann findet das nächste Jumble statt?
           </span>
         </div>
         <div className="col-md-12">
           <label className="labels">Öffnungszeiten</label>
-          <TextField
-            type="time"
-            placeholder="Öffnungszeiten"
-            value="10-16 Uhr"
-          />
+          <TextField type="text" placeholder=" z.B. 9-16 Uhr" value="" />
+
           <span id="timeHelpInline" class="form-text">
             Wie sind die Öffnungszeiten des Jumbles?
           </span>
@@ -48,11 +39,43 @@ export default function JumbleForm({}) {
           <label className="labels"> Website</label>
           <TextField
             type="url"
-            placeholder="Website"
-            value="www.flohschanze.de"
+            placeholder="z.B. www.flohschanze.de"
+            value=""
           />
         </div>
       </div>
     </React.Fragment>
   )
 }
+
+// {mode === 'new' && (
+//   <ButtonGroup>
+//     <Button type="button" onClick={handleCancel}>
+//       abbrechen
+//     </Button>
+//     <Button onClick={handleSaveNewAddress}> speichern </Button>
+//   </ButtonGroup>
+// )}
+// {readOnly && (
+//   <ButtonGroup>
+//     <Button type="button" onClick={() => handleEditAddress(address.id)}>
+//       bearbeiten
+//     </Button>
+//   </ButtonGroup>
+// )}
+// {mode === 'edit' && (
+//   <ButtonGroup>
+//     <Button type="button" onClick={handleCancel}>
+//       abbrechen
+//     </Button>
+//     <Button
+//       type="button"
+//       onClick={() => handleSaveAddressChanges(address.id)}
+//     >
+//       Änderungen speichern
+//     </Button>
+//     <Button type="button" onClick={handleDeleteAddress}>
+//       <Icon src={trash} />
+//     </Button>
+//   </ButtonGroup>
+// )}
