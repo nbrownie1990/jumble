a2a0dd27cbfa1c3756fc0598d85abb7c32aa315e
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-09 18:20:52 +0100

    add crossorigin

--- backend/src/main/java/de/nbrownie/jumblesbackend/controllers/JumbleController.java
+++ backend/src/main/java/de/nbrownie/jumblesbackend/controllers/JumbleController.java
@@ -4,12 +4,14 @@ import de.nbrownie.jumblesbackend.models.Jumble;
 import de.nbrownie.jumblesbackend.repo.JumbleRepository;
 import de.nbrownie.jumblesbackend.services.JumbleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path= "/v1/")
 public class JumbleController {
