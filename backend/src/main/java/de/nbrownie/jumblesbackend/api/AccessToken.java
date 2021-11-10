55af15529ad3d5451557ef84514990e67c052e98
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-10 17:17:23 +0100

    Add Login- and Signup-relevant Classes

---
+++ backend/src/main/java/de/nbrownie/jumblesbackend/api/AccessToken.java
@@ -0,0 +1,15 @@
+package de.nbrownie.jumblesbackend.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccessToken {
+    private String token;
+}
+
