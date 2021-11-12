ad4690e7f131094e82606535ad9c1cad06f0d8c7
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-12 09:12:09 +0100

    change fileNames

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
-
 class NavBar extends React.Component {
   constructor(props) {
     super(props)
--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from '../../components/navbar'
-import MapComponent from '../../components/mapcomponent'
+import MapComponent from '../../components/mapComponent'
 import SearchBar from '../../components/searchBar'
 
 const Home = () => {
