7d2e7a8acb8baec632c91f75369b3f0ee5c56ee2
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-12 09:21:20 +0100

    refactor class into function

--- frontend/src/components/searchbar.jsx
+++ frontend/src/components/searchbar.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <form className="d-flex m-3">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search Adresses or Jumbles"
-          aria-label="Search"
-        />
-        <button className="btn btn-primary" type="submit">
-          Search
-        </button>
-      </form>
-    )
-  }
+export default function SearchBar({}) {
+  return (
+    <form className="d-flex m-3">
+      <input
+        className="form-control me-2"
+        type="search"
+        placeholder="Search Adresses or Jumbles"
+        aria-label="Search"
+      />
+      <button className="btn btn-primary" type="submit">
+        Search
+      </button>
+    </form>
+  )
 }
-
-export default SearchBar
