d674f0caa1289b1aa27e04daab692670492bde0c
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-17 19:05:59 +0100

    replace HashRouter with BrowserRouter

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import 'font-awesome/css/font-awesome.css'
 import './assets/css/style.css'
