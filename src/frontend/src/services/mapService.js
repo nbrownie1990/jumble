c79d51e4a498a9af67f220534952d1e538127885
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-17 19:01:32 +0100

    rm redundant files

--- src/frontend/src/services/mapService.js
+++
@@ -1,9 +0,0 @@
-export const initialViewport = {
-  long: 9.993683,
-  lat: 53.551086,
-  zoom: 12,
-}
-
-export function getInitialViewport() {
-  return initialViewport
-}
--- src/frontend/src/services/profileService.js
+++
@@ -1,7 +0,0 @@
-// export const initialProfileState = {
-//   id: null,
-//   user_img: '',
-//   user_name: '',
-//   user_email: '',
-//   user_text: '',
-// }
