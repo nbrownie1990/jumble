f0946cb0f1578ea2bfbfb9a8a9927ef27c1f1830
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-24 22:41:35 +0100

    add JumbleApi class

---
+++ src/main/java/de/nbrownie/jumbleapplication/api/JumbleApi.java
@@ -0,0 +1,30 @@
+package de.nbrownie.jumbleapplication.api;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import de.nbrownie.jumbleapplication.models.Address;
+import de.nbrownie.jumbleapplication.models.Category;
+import de.nbrownie.jumbleapplication.models.Review;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public class JumbleApi {
+        private Long jumbleId;
+        private String jumbleName;
+        private String jumbleText;
+        private String jumbleImage;
+        private String jumbleDate;
+        private String jumbleTime;
+        private String jumbleWebsite;
+        private Category category;
+        private List<ReviewApi> reviewApiList;
+        private Address address;
+    }
