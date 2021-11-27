d5a42dbbf39715659bcfe0d85e44b98a6810afdb
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-27 20:41:16 +0100

    refactor from class to function

--- src/frontend/src/components/profileForm.js
+++ src/frontend/src/components/profileForm.js
@@ -1,131 +1,136 @@
 import TextField from './textField'
 import TextArea from './textArea'
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
 
-class ProfileForm extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      username: '',
-      email: '',
-      password: '',
-      text: ''
-    }
+export default function ProfileForm({
+mode,
+user,
+handleProfileInputChange,
+handleEditProfile,
+handleSaveNewProfile,
+handleSaveProfileChanges,
+handleCancel,
+handleDeleteUser,
+readOnly,
+}) {
 
-    this.changeNameHandler = this.changeNameHandler.bind(this)
-    this.changeEmailHandler = this.changeEmailHandler.bind(this)
-    this.changePasswordHandler = this.changePasswordHandler.bind(this)
-    this.changeTextHandler = this.changeTextHandler.bind(this)
-  }
-
-  changeNameHandler = e => {
-    this.setState({ name: e.target.value })
-  }
-
-  changeEmailHandler = e => {
-    this.setState({ email: e.target.value })
-  }
-
-  changePasswordHandler = e => {
-    this.setState({ password: e.target.value })
-  }
-  changeTextHandler = e => {
-    this.setState({ text: e.target.value })
-  }
-
-  render() {
     return (
-      <React.Fragment>
-        <div className="col-md-4 border-right">
-          <div className="p-3 py-md-5">
-            <div className="d-flex justify-content-between align-items-center mb-3">
-              <h2 className="text-right">Edit your profile</h2>
-            </div>
-            <div className="row mt-2">
-              <div className="col-md-12">
-                <label className="labels">Nutzername</label>
-                <TextField
-                  placeholder="Nutzername"
-                  name="name"
-                  type="text"
-                  value={this.state.name}
-                  onChange={this.changeNameHandler}
-                />
+        <React.Fragment>
+          <div className="col-md-4 border-right">
+            <div className="p-3 py-md-5">
+              <div className="d-flex justify-content-between align-items-center mb-3">
+                <h2 className="text-right">Edit your profile</h2>
               </div>
-            </div>
+              <div className="row mt-2">
+                <div className="col-md-12">
+                  <label className="labels">Nutzername:</label>
+                  <TextField
+                      disabled={readOnly}
+                      title="Nutzername"
+                      name="username"
+                      type="text"
+                      value={user.username}
+                      onChange={handleProfileInputChange}
+                  />
+                </div>
+              </div>
 
-            <div className="row mt-3">
-              <div className="col-md-12">
-                <label className="labels">E-Mail:</label>
-                <TextField
-                  placeholder="Email"
-                  name="email"
-                  type="text"
-                  value={this.state.email}
-                  onChange={this.changeEmailHandler}
-                />
-                <span id="mailHelpInline" className="form-text">
+              <div className="row mt-3">
+                <div className="col-md-12">
+                  <label className="labels">E-Mail:</label>
+                  <TextField
+                      title="Email"
+                      disabled={readOnly}
+                      name="email"
+                      type="text"
+                      value={user.email}
+                      onChange={handleProfileInputChange} />
+                  <span id="mailHelpInline" className="form-text">
                   Hier eine valide E-Mail-Adresse eintragen.
                 </span>
-              </div>
+                </div>
 
-              <div className="col-md-12">
-                <label className="labels">Passwort</label>
-                <TextField
-                  placeholder="Passwort"
-                  name="password"
-                  type="password"
-                  value={this.state.password}
-                  onChange={this.changePasswordHandler}
-                />
-                <span id="passwordHelpInline" className="form-text">
+                <div className="col-md-12">
+                  <label className="labels">Passwort:</label>
+                  <TextField
+                      title="Passwort"
+                      disabled={readOnly}
+                      name="password"
+                      type="password"
+                      value={user.password}
+                      onChange={handleProfileInputChange} />
+                  <span id="passwordHelpInline" className="form-text">
                   Muss zwischen 8 und 20 Zeichen haben.
                 </span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4 border-right">
-          <div className="p-3 py-md-5">
-            <div className="d-flex flex-column align-items-center text-center">
-              <div className="form-group col-12">
-                <label className="labels mb-2">Erzähl uns etwas von dir</label>
-                <TextArea
-                  rows="10"
-                  placeholder="Erzähl etwas von dir"
-                  name="text"
-                  type="text"
-                  value={this.state.text}
-                  onChange={this.changeTextHandler}
-                ></TextArea>
-                <div className="mt-5 text-center">
-                  <Link
-                    to={`/user`}
-                    className="btn btn-primary profile-button m-3"
-                    type="button"
-                  >
-                    <i className="fas fa-save"></i> Save Profile
-                  </Link>
-                  {/* <Link
-                  to={`/home`}
-                  className="btn btn-warning m-3"
-                  type="button"
-                  data-toggle="tooltip"
-                  title="Delete Profile"
-                >
-                  <i class="fas fa-trash"></i> Delete Profile
-                </Link> */}
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </React.Fragment>
+
+          <div className="col-md-4 border-right">
+            <div className="p-3 py-md-5">
+              <div className="d-flex flex-column align-items-center text-center">
+                <div className="form-group col-12">
+                  <label className="labels mb-2">Erzähl uns etwas von dir</label>
+                  <TextArea
+                      rows="10"
+                      disabled={readOnly}
+                      title="Erzähl etwas von dir"
+                      name="userText"
+                      type="text"
+                      value={user.userText}
+                      onChange={handleProfileInputChange}/>
+                  <div className="mt-5 text-center">
+                    {mode === 'new' && (
+                        <button
+                            className="btn btn-primary profile-button mb-5 m-3"
+                            type="button"
+                            onClick={() => handleSaveNewProfile(user.userId)}
+                        >
+                          <i className="fas fa-save"></i> Save Profile
+                        </button>
+                    )}
+                    {readOnly && (
+                        <button
+                            className="btn btn-primary profile-button mb-5 m-3"
+                            type="button"
+                            onClick={() => handleEditProfile(user.userId)}
+                        >
+                          <i className="fas fa-pen"></i> Edit Profile
+                        </button>
+                    )}
+                    {mode === 'edit' && (
+                        <>
+                          <button
+                              className="btn btn-primary profile-button mb-5 m-3"
+                              type="button"
+                              onClick={() => handleSaveProfileChanges(user.userId)}
+                          >
+                            <i className="fas fa-save"></i> Save Profile
+                          </button>
+                          <button
+                              className="btn btn-warning mb-5 m-3"
+                              type="button"
+                              onClick={() => handleDeleteUser(user.userId)}
+                          >
+                            <i className="fas fa-trash"></i> Delete Profile
+                          </button>
+                        </>
+                    )}
+                    <button
+                        type="button"
+                        className="btn btn-outline-primary"
+                        onClick={() => handleCancel()}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
     )
-  }
 }
 
-export default ProfileForm
