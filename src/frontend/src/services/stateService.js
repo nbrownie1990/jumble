5f45b35f96f36dc1fdade73dd2d77f881391ce64
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-27 20:38:35 +0100

    comment out the inital states to test if there are ref-errors

--- src/frontend/src/services/stateService.js
+++ src/frontend/src/services/stateService.js
@@ -1,36 +1,8 @@
-
-export const jumbleCategoryOptions = [
-    null,
-    'Flohmarkt',
-    'Upcycling',
-    'Donation',
-    'FoodSharing'
-]
-
-export const initialJumbleState = {
-    id: null,
-    image: '',
-    text: '',
-    name: '',
-    category: '',
-    date: '',
-    time: '',
-    website: '',
-}
-
-export const initialAddressState = {
-    id: null,
-    street: '',
-    number: '',
-    zip: '',
-    city: 'Hamburg',
-    country: 'Deutschland',
-}
-
-export const initialViewport = {
-    long: 9.993683,
-    lat: 53.551086,
-    zoom: 12,
+export const initialSignUpState = {
+    userId: null,
+    username: '',
+    email: '',
+    password: '',
 }
 
 export const initialLoginState = {
@@ -38,6 +10,42 @@ export const initialLoginState = {
     password: '',
 }
 
+export const initialProfileState = {
+    userId: null,
+    username: '',
+    email: '',
+    password: '',
+    userText: '',
+    userImage: '',
+    userRole: ''
+}
+
+// export const initialJumbleState = {
+//     jumbleId: null,
+//     jumbleImage: '',
+//     jumbleText: '',
+//     jumbleName: '',
+//     category: '',
+//     jumbleDate: '',
+//     jumbleTime: '',
+//     jumbleWebsite: '',
+// }
+
+
+// export const initialAddressState = {
+//     addressId: null,
+//     addressStreet: '',
+//     addressNumber: '',
+//     addressZip: '',
+//     addressCity: 'Hamburg',
+//     addressCountry: 'Deutschland',
+// }
+
+export const initialViewport = {
+    long: 9.993683,
+    lat: 53.551086,
+    zoom: 12,
+}
 
 
 //
@@ -45,12 +53,3 @@ export const initialLoginState = {
 //     return initialViewport
 // }
 
-
-
-// export const initialProfileState = {
-//   id: null,
-//   user_img: '',
-//   user_name: '',
-//   user_email: '',
-//   user_text: '',
-// }
