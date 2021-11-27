318344e6d715dafc534dab9ed850adb4ee4e3555
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-11-27 20:41:41 +0100

    refactor from const to function

--- src/frontend/src/pages/Profile/profile.js
+++ src/frontend/src/pages/Profile/profile.js
@@ -1,34 +1,50 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from '../../components/navbar'
 import { Link } from 'react-router-dom'
+import {useNavigate, useParams} from "react-router";
+import {getUserById} from "../../services/apiService";
 
-const Profile = () => {
+function Profile () {
+  const navigate = useNavigate()
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  let { userId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    getUserById(userId)
+        .then(user => setUser(user))
+        .catch(error => setError(error))
+        .finally(() => setLoading(false))
+  },[userId])
+
   return (
     <React.Fragment>
       <Navbar />
       <main className="m-md-5 mt-5 mb-5">
         <section className="container w-100 h-100">
+          { loading &&  <p>Data is loading...</p>}
+          { error && <p>There was an error loading your data!</p> }
           <div className="container">
             <div className="row">
               <div className="d-flex flex-column align-items-center text-center">
-                <div className="col-12 mt-5 ">
+                <div key={user.userId} className="col-12 mt-5 ">
                   <img
                     className="rounded-circle user-img "
-                    src="https://images.unsplash.com/photo-1542596594-649edbc13630?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80"
+                    src={user.userImage}
                     alt="Profile"
                   />
                 </div>
-                <h1 className="card-title text-center pt-3 ">Kleiderklara42</h1>
+                <h1 className="card-title text-center pt-3 ">{user.userName}</h1>
                 <div className="col-sm-8 align-self-center ">
                   <div className="card ">
                     <div className="card-body">
                       <p className="m-0 py-2 text-muted ">
-                        Hallo Ihr Lieben! Ich mache regelmäßig Flohmärkte mit,
-                        also kommt vorbei! Pieces from all over the world -
-                        picked and cared with love for you. ♥️
+                        {user.userText} ♥️
                       </p>
                       <Link
-                        to={`/user/edit`}
+                        to={`/user/edit/${userId}`}
                         className="btn btn-outline-dark m-3 float-end"
                         type="button"
                         data-toggle="tooltip"
