40474e2bcbe7b4f19a8ba25086d72925fce7cc27
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-12-03 19:52:14 +0100

    do some cleanup

--- src/frontend/src/components/error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Error({}) {
-  return (
-    <React.Fragment>
-      <p className="error invalid-feedback ">Oh no! This is an error!</p>
-    </React.Fragment>
-  )
-}
