81214e01a23a3e15168440b9ac0629d02a5d8ff1
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-12-15 19:54:38 +0100

    delete console.log

--- src/frontend/src/auth/AuthHeader.js
+++ src/frontend/src/auth/AuthHeader.js
@@ -1,12 +1,9 @@
-import axios from "axios";
-
 export default function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
 
     if (user && user.token) {
-        console.log(user.token);
         return {
-            'Authorization': 'Bearer ' + user.token.toString()
+            'Authorization': 'Bearer '+user.token
         };
     } else {
         return {};
