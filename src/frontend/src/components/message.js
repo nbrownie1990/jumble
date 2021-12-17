39fd4ee64e78836437ed7fbebdbf5f6381276217
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2021-12-17 18:26:18 +0100

    style message-component

--- src/frontend/src/components/message.js
+++ src/frontend/src/components/message.js
@@ -9,24 +9,29 @@ const Message = props => {
             {result && result.map(list => (
         <div key={list.reviewId} className="mine messages">
             <div className="message last row">
-                <div className="col-xs-6 col-12">
-                    <img
-                        className="rounded-circle mt-5"
-                        src={list.user.userImage}
-                        width="50"
-                        alt="This is a user"
-                    />
-                </div>
-                <div className="col-12">
-                    Bewertung von {list.user.username}:
 
-                    <StarRating
-                        ratingValue= {list.reviewRating}
-                    />
+                <div className="row g-3">
+
+                    <div className="col-sm-2">
+                        <img
+                                className="rounded-circle m-2"
+                                src={list.user.userImage}
+                                width="50"
+                                alt="This is a user"
+                        />
+                     </div>
+                    <div className="col-sm-1 m-2">
+                    </div>
+                    <div className="col-sm-8">
+                        Bewertung von {list.user.username}:
+                        <StarRating
+                            ratingValue= {list.reviewRating}
+                        />
+                    </div>
                 </div>
-                <div className="col-12">
-                    {list.reviewText}
-                </div>
+                    <div className="col-12">
+                        {list.reviewText}
+                    </div>
             </div>
         </div>
         ))}
