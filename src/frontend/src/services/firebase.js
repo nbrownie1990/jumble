0f726adb945b7fabca44b3319a56620d26338bb2
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2022-01-11 14:10:42 +0100

    change env-var

--- src/frontend/src/services/firebase.js
+++ src/frontend/src/services/firebase.js
@@ -5,27 +5,27 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCVGVQLHGmjvY16Mviedw3xWgiW_CyHu-8",
-    authDomain: "jumble-113b5.firebaseapp.com",
-    databaseURL: "https://jumble-113b5-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "jumble-113b5",
-    storageBucket: "jumble-113b5.appspot.com",
-    messagingSenderId: "565820066963",
-    appId: "1:565820066963:web:ba024505a6566125c79166",
-    measurementId: "G-ZM5PM6L8RG"
-};
-
 // const firebaseConfig = {
-//     apiKey:`${process.env.FIREBASE_CONFIG_API}`,
-//     authDomain: `${process.env.FIREBASE_CONFIG_AUTH}`,
-//     databaseURL: `${process.env.FIREBASE_CONFIG_URL}`,
-//     projectId: `${process.env.FIREBASE_CONFIG_ID}`,
-//     storageBucket: `${process.env.FIREBASE_CONFIG_BUCKET}`,
-//     messagingSenderId: `${process.env.FIREBASE_CONFIG_SENDER_ID}`,
-//     appId: `${process.env.FIREBASE_CONFIG_APP_ID}`,
-//     measurementId: `${process.env.FIREBASE_CONFIG_MEASURE_ID}`
+//     apiKey: "AIzaSyCVGVQLHGmjvY16Mviedw3xWgiW_CyHu-8",
+//     authDomain: "jumble-113b5.firebaseapp.com",
+//     databaseURL: "https://jumble-113b5-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "jumble-113b5",
+//     storageBucket: "jumble-113b5.appspot.com",
+//     messagingSenderId: "565820066963",
+//     appId: "1:565820066963:web:ba024505a6566125c79166",
+//     measurementId: "G-ZM5PM6L8RG"
 // };
+
+const firebaseConfig = {
+    apiKey:`${process.env.REACT_APP_FIREBASE_CONFIG_API}`,
+    authDomain: `${process.env.REACT_APP_FIREBASE_CONFIG_AUTH}`,
+    databaseURL: `${process.env.REACT_APP_FIREBASE_CONFIG_URL}`,
+    projectId: `${process.env.REACT_APP_FIREBASE_CONFIG_ID}`,
+    storageBucket: `${process.env.REACT_APP_FIREBASE_CONFIG_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_FIREBASE_CONFIG_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_FIREBASE_CONFIG_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_FIREBASE_CONFIG_MEASURE_ID}`
+};
 
 
 // Initialize Firebase
