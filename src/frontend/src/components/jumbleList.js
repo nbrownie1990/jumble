41b97747c7c55793dfad0cbc890574a2bcb6f066
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2022-01-13 15:18:27 +0100

    try out responsive-design-changes

--- src/frontend/src/components/jumbleList.js
+++ src/frontend/src/components/jumbleList.js
@@ -6,46 +6,82 @@ const JumbleList = props => {
     const { items, onItemSelected} = props
 
   return (
-             <div className="container">
-                 {items.map(item => (
-                 <div key={item.jumbleId} className="card m-2">
-                 <div className="card-body">
-                     <div className="row g-0">
-                         <div className="col-sm-6 col-md-8">
-                         <div className="d-none d-sm-block">
-                         <img src={item.jumbleImage} className="img-fluid rounded-start w-50" alt="This is a jumble image" />
-                         </div>
-                         <h5 className="text-primary text-start align-self-start mb-0">
-                            {item.jumbleName}
-                         </h5>
-                    <p className="m-0 py-2 text-muted text-start" key={item.address?.addressId}>
-                        {item.address?.addressStreet} {' '}
-                         {item.address?.addressNumber}
-                        <br/>
-                         {item.address?.addressZip}{' '}
-                         {item.address?.addressCity}{' '}
-                    </p>
-                </div>
+      <div className="container">
+          {items.map(item => (
+              <div key={item.jumbleId} className="card mb-3" style="max-width: 540px;">
+          <div className="row no-gutters">
+              <div className="col-md-4">
+                      <img src={item.jumbleImage} className="card-img" alt="This is a jumble image" />
+              </div>
+              <div className="col-md-8">
+                  <div className="card-body">
+                      <div className="d-none d-sm-block">
+                          <StarRating
+                              className="m-auto"
+                              ratingValue="3"/>
+                      </div>
+                      <h5 className="card-title">{item.jumbleName}</h5>
+                      <p className="card-text" key={item.address?.addressId}>
+                          {item.address?.addressStreet} {' '}
+                          {item.address?.addressNumber}
+                          <br/>
+                          {item.address?.addressZip}{' '}
+                          {item.address?.addressCity}{' '}
+                      </p>
+                      <p className="card-text">
+                          <Link
+                              to={`/jumbles/${item.jumbleId}`}
+                              onClick={() => onItemSelected(item)}
+                              className="btn btn-primary mt-auto"
+                          >
+                              Details
+                          </Link>
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
 
-                             <div className="col-6 col-md-4">
-                                 <div className="col-6 col-md-4 d-none d-sm-block">
-                                 <StarRating
-                                 className="m-auto"
-                                 ratingValue="3"/>
-                                 </div>
-                             <div className="text-end">
-                                 <Link
-                                     to={`/jumbles/${item.jumbleId}`}
-                                     onClick={() => onItemSelected(item)}
-                                     className="btn btn-primary mt-auto"
-                                 >
-                                     Details
-                                 </Link>
-                             </div>
-                             </div>
-                     </div>
-           </div>
-        </div>
+        //
+        //      <div className="container">
+        //          {items.map(item => (
+        //          <div key={item.jumbleId} className="card m-2">
+        //          <div className="card-body">
+        //              <div className="row g-0">
+        //                  <div className="col-sm-6 col-md-8">
+        //                  <div className="d-none d-sm-block">
+        //                  <img src={item.jumbleImage} className="img-fluid rounded-start w-50" alt="This is a jumble image" />
+        //                  </div>
+        //                  <h5 className="text-primary text-start align-self-start mb-0">
+        //                     {item.jumbleName}
+        //                  </h5>
+        //             <p className="m-0 py-2 text-muted text-start" key={item.address?.addressId}>
+        //                 {item.address?.addressStreet} {' '}
+        //                  {item.address?.addressNumber}
+        //                 <br/>
+        //                  {item.address?.addressZip}{' '}
+        //                  {item.address?.addressCity}{' '}
+        //             </p>
+        //         </div>
+        //              <div className="col-6 col-md-4">
+        //                          <div className="col-6 col-md-4 d-none d-sm-block">
+        //                          <StarRating
+        //                          className="m-auto"
+        //                          ratingValue="3"/>
+        //                          </div>
+        //                      <div className="text-end">
+        //                          <Link
+        //                              to={`/jumbles/${item.jumbleId}`}
+        //                              onClick={() => onItemSelected(item)}
+        //                              className="btn btn-primary mt-auto"
+        //                          >
+        //                              Details
+        //                          </Link>
+        //                      </div>
+        //                      </div>
+        //              </div>
+        //    </div>
+        // </div>
       ))}
     </div>
   )
