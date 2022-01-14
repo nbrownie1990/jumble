fdf7d75d0d500737aa66929749f4040838bd2d62
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2022-01-14 20:33:29 +0100

    try to add more performance with inline-bg-img

--- src/frontend/src/pages/Start/start.js
+++ src/frontend/src/pages/Start/start.js
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Start = () => {
   return (
-    <main className="d-flex text-center text-white m-md-5 mt-5 mb-5 min-vh-100">
+
+    <main className="d-flex text-center text-white m-md-5 mt-5 mb-5 min-vh-100"
+          style={
+      {backgroundImage: "url(" + "https://firebasestorage.googleapis.com/v0/b/jumble-113b5.appspot.com/o/footer%2Fabstractjumble-sm.webp?alt=media&token=d3783375-0e03-47cd-9c93-7100b4185f25" + ")",
+        backgroundAttachment: "fixed",
+        backgroundRepeat: "no-repeat"}}>
       <section className="container w-100 p-3 flex-column align-self-center">
         <div className="px-3 ">
           <h1>Jumble - the right place for your stuff</h1>
