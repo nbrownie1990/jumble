7c1f00b8b3d322764321c51615ac16cf69625c69
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2022-01-14 19:36:33 +0100

    try out suspense and lazy for performance

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,5 @@
 // -- React and related libs
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, Suspense} from 'react'
 import {Route, Routes} from 'react-router-dom'
 
 // -- Custom Components
@@ -16,16 +16,28 @@ import Signup from './pages/SignUp/signup'
 import Impressum from './pages/Impressum/impressum'
 import Footer from './components/footer'
 
-import Home from './pages/Home/home'
-import Jumble from './pages/Jumble/jumble'
-import EditJumble from './pages/Jumble/editJumble'
-import AddJumble from './pages/Jumble/addJumble'
-import EditProfile from './pages/Profile/editProfile'
-import Category from './pages/Category/category'
-import Categories from './pages/Categories/categories'
-import AllJumbles from "./pages/Jumble/allJumbles"
-import Profile from './pages/Profile/profile'
-import MyProfile from "./pages/Profile/myProfile"
+//import Home from './pages/Home/home'
+//import Jumble from './pages/Jumble/jumble'
+//import EditJumble from './pages/Jumble/editJumble'
+//import AddJumble from './pages/Jumble/addJumble'
+//import AllJumbles from "./pages/Jumble/allJumbles"
+//import Category from './pages/Category/category'
+//import Categories from './pages/Categories/categories'
+//import Profile from './pages/Profile/profile'
+//import MyProfile from "./pages/Profile/myProfile"
+//import EditProfile from './pages/Profile/editProfile'
+
+
+const Home = React.lazy(() => import("./pages/Home/home"));
+const Jumble = React.lazy(() => import("./pages/Jumble/jumble"));
+const EditJumble = React.lazy(() => import("./pages/Jumble/editJumble"));
+const AddJumble = React.lazy(() => import("./pages/Jumble/addJumble"));
+const AllJumbles = React.lazy(() => import("./pages/Jumble/allJumbles"));
+const Profile = React.lazy(() => import("./pages/Profile/profile"));
+const MyProfile = React.lazy(() => import("./pages/Profile/myProfile"));
+const EditProfile = React.lazy(() => import("./pages/Profile/editProfile"));
+const Category = React.lazy(() => import("./pages/Category/category"));
+const Categories = React.lazy(() => import("./pages/Categories/categories"));
 
 
 // -- Component Styles
@@ -53,16 +65,86 @@ const [currentUser, setCurrentUser] = useState(undefined);
           <Route path="/logoutnow" element={<Logout />} />
           <Route path="/notallowed" element={<NotAllowed />}/>
           <Route element={<ProtectedRoutes/>}>
-            <Route path="/jumbles/edit/:jumbleId" element={<EditJumble/>}/>
-            <Route path="/jumbles/:jumbleId" element={<Jumble />} />
-            <Route path="/jumbles/new" element={<AddJumble />} />
-            <Route path="/jumbles/getall" element={<AllJumbles />} />
-            <Route path="/categories/:categoryId" element={<Category />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/user/:id" element={<Profile />} />
-            <Route path="/user/edit/:id" element={<EditProfile/>} />
-            <Route path="/user/me" element={<MyProfile/>} />
-            <Route path="/home" element={<Home />} />
+              <Route
+                  path="/jumbles/:jumbleId"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <Jumble/>
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/jumbles/edit/:jumbleId"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <EditJumble />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/jumbles/new"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <AddJumble />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/jumbles/getall"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <AllJumbles />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/categories/:categoryId"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <Category />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/categories"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <Categories />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/user/:id"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <Profile />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/user/edit/:id"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <EditProfile />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/user/me"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <MyProfile />
+                      </Suspense>
+                  }
+              />
+              <Route
+                  path="/home"
+                  element={
+                      <Suspense fallback={<>...</>}>
+                          <Home />
+                      </Suspense>
+                  }
+              />
             <Route path="*" element={<NotFound />} />
           </Route>
       </Routes>
