1236276d6ea8da6d184e8ad9c02b1bf36362298f
nbrownie <82531854+nbrownie1990@users.noreply.github.com>
2022-01-18 14:59:44 +0100

    add tests

--- src/test/java/de/nbrownie/jumbleapplication/JumblesbackendApplicationTests.java
+++ src/test/java/de/nbrownie/jumbleapplication/JumblesbackendApplicationTests.java
@@ -1,15 +1,13 @@
-//package de.nbrownie.jumbleapplication;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class JumblesbackendApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//		Assertions.fail("This is a fail");
-//	}
-//
-//}
+package de.nbrownie.jumbleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumblesbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+		}
+
+}
